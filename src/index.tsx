d29b7b54036866d9b43d98540031b5b5432c8f46
Pavelkv94 <pavel.kv94@yandex.by>
2021-04-21 17:21:50 +0300

    fix: import error - fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AppWithRedux from './AppWithReducers';
+import AppWithRedux from './AppWithRedux';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 
