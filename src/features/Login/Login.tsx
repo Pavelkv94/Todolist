fc0992c6db4d50d4e442801b622bc96c6d185f21
pavel.kv94 <pavel.kv94@yandex.by>
2021-06-22 00:02:50 +0300

    refactor: refactor form component

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -31,6 +31,7 @@ export const Login = () => {
         },
         onSubmit: values => {
             alert(JSON.stringify(values));
+            formik.resetForm();
         },
     })
 
@@ -53,28 +54,20 @@ export const Login = () => {
                         <TextField
                             label="Email"
                             margin="normal"
-                            name="email"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.email}
+                            {...formik.getFieldProps('email')}
                         />
-                        {formik.errors.email ? <div style={{ color: 'red' }}>{formik.errors.email}</div> : null}
+                        {formik.touched.email &&
+                            formik.errors.email ? <div style={{ color: 'red' }}>{formik.errors.email}</div> : null}
                         <TextField
-                            type="password"
                             label="Password"
                             margin="normal"
-                            onBlur={formik.handleBlur}
-                            name="password"
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
+                            {...formik.getFieldProps('password')}
                         />
-                        {formik.errors.password ? <div style={{ color: 'red' }}>{formik.errors.password}</div> : null}
+                        {formik.touched.password && formik.errors.password ? <div style={{ color: 'red' }}>{formik.errors.password}</div> : null}
                         <FormControlLabel
                             label={'Remember me'}
                             control={<Checkbox
-                                name="rememberMe"
-                                onChange={formik.handleChange}
-                                checked={formik.values.rememberMe}
+                                {...formik.getFieldProps('rememberMe ')}
                             />}
 
                         />
